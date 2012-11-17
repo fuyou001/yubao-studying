0f03b537f07502165b834b31a31c90ca4ecfad3c
yubaofu <fuyou001@gmail.com>
2012-11-17 11:34:39 +0800

    添加diff工具

---
+++ J2SE/src/main/java/com/fuyou/reflect/Disable.java
@@ -0,0 +1,12 @@
+package com.fuyou.reflect;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author yubaofu created on  12-11-9 上午10:09
+ * @version $Id$
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Disable {
+}
