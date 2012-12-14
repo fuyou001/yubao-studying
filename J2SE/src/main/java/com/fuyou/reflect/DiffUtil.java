ac5ec99f0a9dde0d5c037fc958d97ae4a646db96
yubaofu <fuyou001@gmaiil.com>
2012-12-14 17:48:19 +0800

    修复bug

--- J2SE/src/main/java/com/fuyou/reflect/DiffUtil.java
+++ J2SE/src/main/java/com/fuyou/reflect/DiffUtil.java
@@ -3,12 +3,6 @@
  */
 package com.fuyou.reflect;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
-import org.apache.commons.lang3.time.DateUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -16,15 +10,22 @@ import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.Date;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
- * @author yubaofu created on  12-11-9 上午10:11
+ * @author yubaofu created on 12-11-9 上午10:11
  * @version $Id$
  */
 public class DiffUtil {
     private static final Log logger = LogFactory.getLog(DiffUtil.class);
     private static final String separator = " ";
 
-    public static String diff(Object oldObj, Object newObj) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ParseException {
+    public static String diff(Object oldObj, Object newObj) throws NoSuchMethodException, InvocationTargetException,
+            IllegalAccessException, ParseException {
         StringBuilder sb = new StringBuilder();
         if (oldObj == null || newObj == null)
             return sb.toString();
@@ -33,7 +34,8 @@ public class DiffUtil {
         Method[] newMethods = oldObj.getClass().getDeclaredMethods();
         for (java.lang.reflect.Field field : oldFields) {
             Disable disable = field.getAnnotation(Disable.class);
-            if (disable != null) continue;
+            if (disable != null)
+                continue;
             String filedName = field.getName();
             Class filedClass = field.getType();
 
@@ -48,20 +50,20 @@ public class DiffUtil {
             Object newValue = newCurrentMethod.invoke(newObj, null);
 
             if (logger.isDebugEnabled()) {
-                logger.debug("filedName:" + filedName + " filedClass:" + filedClass + " method:" + oldCurrentMethod.getName()
-                        + "valueClass:" + (oldValue == null ? null : oldValue.getClass().getName())
-                        + "oldValue:" + oldValue + " newValue:" + newValue);
+                logger.debug("filedName:" + filedName + " filedClass:" + filedClass + " method:"
+                        + oldCurrentMethod.getName() + "valueClass:"
+                        + (oldValue == null ? null : oldValue.getClass().getName()) + "oldValue:" + oldValue
+                        + " newValue:" + newValue);
             }
 
             StringBuilder diffValueLog = diffValueLog(oldValue, newValue, field);
             if (StringUtils.isNotEmpty(diffValueLog)) {
-                if (sb.length() > 1) sb.append(System.getProperty("line.separator"));
+                if (sb.length() > 1)
+                    sb.append(System.getProperty("line.separator"));
                 sb.append(diffValueLog);
             }
 
         }
-
-
 
         return sb.toString();
     }
@@ -71,7 +73,7 @@ public class DiffUtil {
         if (oldValue == newValue) {
             return sb;
         }
-        if (oldValue != null && oldValue.getClass().getName().contains("[")) { //数组先忽略
+        if (oldValue != null && oldValue.getClass().getName().contains("[")) { // 数组先忽略
             return sb;
         }
         String filedName = field.getName();
@@ -79,15 +81,16 @@ public class DiffUtil {
         String name = displayDiffField == null ? filedName : displayDiffField.name();
         if (oldValue == null || newValue == null) {
             sb.append(name + ":" + format(oldValue, field) + "改为" + format(newValue, field));
-        }else if (!equals(oldValue, newValue, field)) {
+        } else if (!equals(oldValue, newValue, field)) {
             sb.append(name + ":" + format(oldValue, field) + "改为" + format(newValue, field));
         }
         return sb;
     }
 
     private static String format(Object value, Field field) {
-        if(value==null) return separator;
-        if(value instanceof  Date){
+        if (value == null)
+            return separator;
+        if (value instanceof Date) {
             String pattern = field.getAnnotation(DiffField.class).datePatter();
             return DateFormatUtils.format((Date) value, pattern);
         }
@@ -111,9 +114,11 @@ public class DiffUtil {
     }
 
     private static Method getMethodByName(Method[] oldMethods, String filedName) {
-        String methodName = filedName.substring(0, 1).toUpperCase() + filedName.substring(1);
+        String  methodNameSuffix = filedName.substring(0, 1).toUpperCase() + filedName.substring(1);
         for (Method method : oldMethods) {
-            if (method.getName().equals("get" + methodName) || method.getName().equals("is" + methodName)) {
+            String methodName = method.getName();
+            boolean isMatch = methodName.equals("get" + methodNameSuffix) ||methodName.equals(filedName) || methodName.equals("is" + methodNameSuffix);
+            if (isMatch) {
                 return method;
             }
         }
